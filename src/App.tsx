cc45f52c806b4105d0f82467b0c62ba360a6a402
Joaquin <JoaquinJSX>
2025-05-15 21:48:53 -0500

    Adding all files

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Panel from './AppComponents/DevConnectPanel';
+
+function App() {
+
+  document.title = "DevConnect - Find your community";
+
+  return (
+    <div>
+      <Panel />
+    </div>
+  );
+}
+
+export default App;
